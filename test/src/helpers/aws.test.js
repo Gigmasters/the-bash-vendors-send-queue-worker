bd0eac6312b77773d4375bf61ce8955d842b4a27
mou <mou@xogrp.com>
2022-08-30 17:38:34 +0800

    add jest package

---
+++ test/src/helpers/aws.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('test', () => {
+  it('should ', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ test/src/utils/converter.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('utils', () => {
+  it('should ', () => {
+    expect(1).toBe(1);
+  });
+});

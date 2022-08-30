04fcd3b4b108dd0c0cfec1b2a339f2f5a2cd485c
mou <mou@xogrp.com>
2022-08-30 17:38:34 +0800

    add jest package

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('index', () => {
+  it('should ', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ test/src/jobs/index.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('jobs', () => {
+  it('should ', () => {
+    expect(1).toBe(1);
+  });
+});
